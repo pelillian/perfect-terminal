7408a7e263c00cbb0348b3c940022c654b285967
Peter Lillian <p@peterlillian.com>
2023-11-04 00:25:51 -0600

    Create setup_debian.sh

---
+++ setup_debian.sh
@@ -0,0 +1,10 @@
+sudo apt update
+mkdir -p ~/.config/kak/
+ln -s ~/git/perfect-terminal/kakrc ~/.config/kak/kakrc
+ln -s ~/git/perfect-terminal/.tmux.conf ~/.tmux.conf
+ln -s ~/git/perfect-terminal/.inputrc ~/.inputrc
+touch ~/.bashrc
+cat ~/git/perfect-terminal/append.bashrc >> ~/.bashrc
+
+sudo apt install -y htop tree kakoune
+echo 'set completion-ignore-case On' | sudo tee -a /etc/inputrc
