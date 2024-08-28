cad390568419b28cf3d21fbbd9b3f1ef4e8e7851
Peter Lillian <p@peterlillian.com>
2024-08-27 18:04:14 -0600

    Create setup_linux_minimal.sh

---
+++ setup_linux_minimal.sh
@@ -0,0 +1,9 @@
+mkdir -p ~/.config/kak/
+ln -s ~/git/perfect-terminal/kakrc ~/.config/kak/kakrc
+ln -s ~/git/perfect-terminal/.tmux.conf ~/.tmux.conf
+ln -s ~/git/perfect-terminal/.inputrc ~/.inputrc
+touch ~/.bashrc
+cat ~/git/perfect-terminal/append.bashrc >> ~/.bashrc
+
+echo 'set completion-ignore-case On' | sudo tee -a /etc/inputrc
+sudo apt install -y htop tree kakoune tmux
