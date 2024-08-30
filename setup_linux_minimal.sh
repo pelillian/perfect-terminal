962e54cd1cf295085eb7543276e9bca1bb540854
Peter Lillian <p@peterlillian.com>
2024-08-29 22:59:14 -0600

    Don't install kak automatically

--- setup_linux_minimal.sh
+++ setup_linux_minimal.sh
@@ -6,4 +6,4 @@ touch ~/.bashrc
 cat ~/git/perfect-terminal/append.bashrc >> ~/.bashrc
 
 echo 'set completion-ignore-case On' | sudo tee -a /etc/inputrc
-sudo apt install -y htop tree kakoune tmux
+sudo apt install -y htop tree tmux
