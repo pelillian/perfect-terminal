444db701c010556ab87d33b3fa22dadb78f66539
Peter Lillian <p@peterlillian.com>
2026-06-02 14:21:06 -0700

    zellij layout

--- setup_common.sh
+++ setup_common.sh
@@ -2,14 +2,16 @@
 
 cd
 mkdir -p ~/.config/kak/
-mkdir -p ~/.config/zellij/
+mkdir -p ~/.config/zellij/layouts/
 mv ~/.config/kak/kakrc ~/.config/kak/kakrc.old
 mv ~/.config/zellij/config.kdl ~/.config/zellij/config.kdl.old
+mv ~/.config/zellij/layouts/compact-top.kdl ~/.config/zellij/layouts/compact-top.kdl.old
 mv ~/.zshrc ~/.zshrc.old
 mv ~/.tmux.conf ~/.tmux.conf.old
 mv ~/.inputrc ~/.inputrc.old
 ln -s ~/git/perfect-terminal/kakrc ~/.config/kak/kakrc
 ln -s ~/git/perfect-terminal/zellij.config.kdl ~/.config/zellij/config.kdl
+ln -s ~/git/perfect-terminal/zellij.layout.kdl ~/.config/zellij/layouts/compact-top.kdl
 ln -s ~/git/perfect-terminal/.zshrc ~/.zshrc
 ln -s ~/git/perfect-terminal/.tmux.conf ~/.tmux.conf
 ln -s ~/git/perfect-terminal/.inputrc ~/.inputrc
--- setup_minimal.sh
+++ setup_minimal.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.config/helix/
-mkdir -p ~/.config/zellij/
+mkdir -p ~/.config/zellij/layouts/
 ln -s ~/git/perfect-terminal/helix.config.toml ~/.config/helix/config.toml
 ln -s ~/git/perfect-terminal/zellij.config.kdl ~/.config/zellij/config.kdl
+ln -s ~/git/perfect-terminal/zellij.layout.kdl ~/.config/zellij/layouts/compact-top.kdl
 ln -s ~/git/perfect-terminal/.tmux.conf ~/.tmux.conf
 ln -s ~/git/perfect-terminal/.inputrc ~/.inputrc
 touch ~/.bashrc
